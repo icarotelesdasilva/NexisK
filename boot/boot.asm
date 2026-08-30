8923effe0cb9fbc677983a7e302762e5b4f5da10
icarotelesdasilva <telesicaro04@gmail.com>
2026-08-30 11:59:14 -0300

    boot: add E820 memory map detection

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,23 +1,27 @@
+; Nexis Kernel
+; Copyright (C) 2026 Ícaro Teles da Silva Ribeiro
+; Author: @icarotelesdasilva
+; SPDX-License-Identifier: GPL-2.0-only
 
- ; Nexis Kernel
- ; Copyright (C) 2026 Ícaro Teles da Silva Ribeiro
- ; Author: @icarotelesdasilva
- ; SPDX-License-Identifier: GPL-2.0-only
- 
+[BITS 16]
+[ORG 0x7C00]
 
-bits 16
-org 0x7C00
 
-KERNEL_SEG      equ 0x1000
-KERNEL_OFFSET   equ 0x0000
+KERNEL_SEG          equ 0x1000
+KERNEL_OFFSET       equ 0x0000
 
-CODE_SEG        equ 0x08
-DATA_SEG        equ 0x10
+MEMORY_MAP          equ 0x5000
+MEMORY_MAP_COUNT    equ 0x4FF0
+MEMORY_MAP_MAX      equ 32
+
+CODE_SEG            equ 0x08
+DATA_SEG            equ 0x10
 
 %include "kernel_sectors.inc"
 
-start:
 
+
+start:
     cli
 
     xor ax, ax
@@ -29,39 +33,98 @@ start:
     mov [boot_drive], dl
 
 
+    call get_memory_map
+    jc memory_map_error
+
+
     mov ax, KERNEL_SEG
     mov es, ax
     mov bx, KERNEL_OFFSET
 
-    mov ah, 0x02            
-    mov al, KERNEL_SECTORS  
-    mov ch, 0               
-    mov cl, 2               
-    mov dh, 0               
+    mov ah, 0x02
+    mov al, KERNEL_SECTORS
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
     mov dl, [boot_drive]
 
     int 0x13
 
     jc disk_error
 
+
+
     cli
-
-
     lgdt [gdt_descriptor]
 
-
+    
     mov eax, cr0
     or eax, 0x00000001
     mov cr0, eax
 
-    
     jmp CODE_SEG:init_pm
 
 
-bits 32
+get_memory_map:
+    xor ebx, ebx
+
+    xor ax, ax
+    mov [MEMORY_MAP_COUNT], ax
+
+    xor ax, ax
+    mov es, ax
+    mov di, MEMORY_MAP
+
+.next_entry:
+
+    mov ax, [MEMORY_MAP_COUNT]
+    cmp ax, MEMORY_MAP_MAX
+    jae .done
+
+    mov eax, 0x0000E820
+    mov edx, 0x534D4150
+    mov ecx, 24
+
+    push di
+
+    int 0x15
+
+    pop di
+
+    jc .error
+
+    cmp eax, 0x534D4150
+    jne .error
+
+    cmp ecx, 20
+    jb .error
+
+    inc word [MEMORY_MAP_COUNT]
+
+    add di, 24
+
+    test ebx, ebx
+    jz .done
+
+    jmp .next_entry
+
+
+.done:
+    cmp word [MEMORY_MAP_COUNT], 0
+    je .error
+
+    clc
+    ret
+
+
+.error:
+    stc
+    ret
+
+
+[BITS 32]
 
 init_pm:
-
     mov ax, DATA_SEG
 
     mov ds, ax
@@ -70,14 +133,38 @@ init_pm:
     mov gs, ax
     mov ss, ax
 
-    
     mov esp, 0x00090000
-
 
     jmp 0x00010000
 
 
-bits 16
+
+[BITS 16]
+
+memory_map_error:
+    xor ax, ax
+    mov ds, ax
+
+    mov si, memory_map_error_msg
+
+.print:
+    lodsb
+
+    test al, al
+    jz .halt
+
+    mov ah, 0x0E
+    mov bh, 0x00
+    int 0x10
+
+    jmp .print
+
+.halt:
+    cli
+    hlt
+    jmp .halt
+
+
 
 disk_error:
     xor ax, ax
@@ -103,11 +190,15 @@ disk_error:
     jmp .halt
 
 
+
 boot_drive:
     db 0
 
 disk_error_msg:
     db "BOOT READ DISK ERROR", 0
+
+memory_map_error_msg:
+    db "E820 ERROR", 0
 
 
 
@@ -146,3 +237,4 @@ gdt_descriptor:
 times 510 - ($ - $$) db 0
 
 dw 0xAA55
+
--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,36 +1,44 @@
+#include "stdint.h"
 
 /*
- 
  * Nexis Kernel
  * Copyright (C) 2026 Ícaro Teles da Silva Ribeiro
  * Author: @icarotelesdasilva
  * SPDX-License-Identifier: GPL-2.0-only
- 
  */
 
 extern void serial_print(const char *str);
 extern void vga_clear(void);
 extern void vga_print(char *str);
+
 #include "interrupts/io.h"
 #include "interrupts/pic.h"
+
 extern void pic_remap(uint8_t offset1, uint8_t offset2);
 extern void kernel_panic(char *str);
+extern void serial_print_hex(uint32_t n);
 void init_pit(uint32_t frequency);
 extern void idt_install(void);
 
 
-
 void kmain(void)
 {
+    uint16_t count = *(volatile uint16_t *)0x4FF0;
 
-pic_remap(0x20, 0x28); 
-idt_install();
-init_pit(1000);
-vga_clear();
-serial_print("kernel alive.");
-vga_print("Kernel alive.");
+    pic_remap(0x20, 0x28);
+    idt_install();
+    init_pit(1000);
 
+    vga_clear();
 
-for (;;)
-asm volatile ("sti");
+    serial_print("kernel alive.\n");
+
+    serial_print("E820 entries: ");
+    serial_print_hex(count);
+    serial_print("\n");
+
+    vga_print("Kernel alive.");
+
+    for (;;)
+        asm volatile ("sti");
 }
