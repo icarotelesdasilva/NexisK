9703644d9fc8076d964395e4252efe49035db1da
icarotelesdasilva <telesicaro04@gmail.com>
2026-08-30 18:22:22 -0300

    feat(gdt): add ring 3 support and TSS

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -5,9 +5,13 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
-#include "stdint.h"
+
+ #include "stdint.h"
+
 #include "memory/pmm.h"
+#include "vmm/vmm.h"
 #include "interrupts/pic.h"
+
 
 extern void serial_print(const char *str);
 extern void serial_print_hex(uint32_t n);
@@ -17,20 +21,54 @@ extern void vga_print(char *str);
 
 extern void pic_remap(uint8_t offset1, uint8_t offset2);
 extern void idt_install(void);
+extern void init_gdt(void);
 extern void init_pit(uint32_t frequency);
+
+
+extern void mouse_register_interrupt(void);
+extern void ps2_mouse_init(int screen_width, int screen_height);
+extern void unmask_mouse_irq(void);
+
 
 void kmain(void)
 {
     uint16_t count = *(volatile uint16_t *)0x4FF0;
 
+
+
+    init_gdt();
+
+
+
     pic_remap(0x20, 0x28);
+
+
+    pmm_init();
+    vmm_init();
+
+
+
     idt_install();
+
+
+    init_pit(1000);
+
+
+
+    mouse_register_interrupt();
+    ps2_mouse_init(800, 600);
+    unmask_mouse_irq();
+
+
 
     vga_clear();
 
     serial_print("kernel alive.\n");
 
-    pmm_init();
+    serial_print("E820 entries: ");
+    serial_print_hex(count);
+    serial_print("\n");
+
 
     uint32_t page1 = pmm_alloc_page();
     uint32_t page2 = pmm_alloc_page();
@@ -43,7 +81,9 @@ void kmain(void)
     serial_print_hex(page2);
     serial_print("\n");
 
+
     pmm_free_page(page1);
+
 
     uint32_t page3 = pmm_alloc_page();
 
@@ -51,13 +91,10 @@ void kmain(void)
     serial_print_hex(page3);
     serial_print("\n");
 
-    init_pit(1000);
 
-    serial_print("E820 entries: ");
-    serial_print_hex(count);
-    serial_print("\n");
 
     vga_print("Kernel alive.");
+
 
     for (;;)
         asm volatile ("sti");
