25e9eeb4fffc0e9f27551a48303d266c7a1822c0
icarotelesdasilva <telesicaro04@gmail.com>
2026-09-06 15:14:34 -0300

    feat: implement initial PMM memory map integration

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -6,7 +6,7 @@
  */
 
 #include <stdint.h>
-
+#include "pmm.h"
 #include "interrupts/pic.h"
 #include "memory/memory_learn.h"
 
--- kernel/memory/pmm.h
+++ kernel/memory/pmm.h
@@ -3,7 +3,7 @@
 
 #include "memory_learn.h"
 
-void pmm_page_block(void *address);
 void pmm_init(void);
+void pmm_alloc(void);
 
 #endif 
